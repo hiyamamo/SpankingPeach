655b586015f1f3f61e050ad1720287987c12fba8
hyamamoto <h1r0ki_yamam0t0@yahoo.co.jp>
2014-07-28 08:56:08 +0900

    桃と背景を描画

--- app/src/main/java/com/example/spankingpeach/MainActivity.java
+++ app/src/main/java/com/example/spankingpeach/MainActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.spankingpeach.screen.MainSurfaceView;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new MainSurfaceView(this));
     }
 
 
