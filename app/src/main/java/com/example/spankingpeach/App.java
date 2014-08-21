1e8fabdedc2336746f117989030a13a9bfade92a
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2014-08-21 20:51:45 +0900

    Appファイルあげ忘れ

---
+++ app/src/main/java/com/example/spankingpeach/App.java
@@ -0,0 +1,31 @@
+package com.example.spankingpeach;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
+/**
+ * Created by dev on 14/08/07.
+ */
+public class App extends Application{
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        context = getApplicationContext();
+    }
+    public static Context getContext(){
+        return context;
+    }
+    public static Point getWindowSize(){
+        WindowManager wm= (WindowManager)context.getSystemService(context.WINDOW_SERVICE) ;
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+
+        return size;
+    }
+
+}
